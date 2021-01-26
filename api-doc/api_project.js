b666d3c621822da2910a0488a8ba36fd77969e80
bobbaicloudwithpants@gmail.com <bobbaicloudwithpants@gmail.com>
2021-01-26 22:09:49 +0800

    init

---
+++ api-doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "BlockChainAPI",
+  "version": "1.0.0",
+  "description": "区块链后端接口文档",
+  "title": "BlockChainAPI",
+  "url": "39.99.211.12:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-01-26T13:57:04.897Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# SupplyChain 文档仓库
+- api-doc 为后端api文档
+- report 为设计报告
